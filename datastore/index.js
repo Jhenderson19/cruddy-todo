a6f7922c21e73c54bb62f04df1f528b6333d8a9d
Gkolman <Gkolman17@gmail.com>
2021-01-05 19:38:40 -0800

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -91,14 +91,22 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+
+  fs.unlink(path.join(exports.dataDir, id + '.txt'), (err) => {
+    if (err) {
+      console.log('error -> ', err);
+    }
     callback();
-  }
+
+  } );
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
